9e9b53da7d05bb26be62d45247c444bc7fd92841
wilhellmina <x@wakiga.nl>
2021-10-30 23:41:42 +0900

    既にルールが追加されている場合有効にならないバグ修正

--- drop-twitter.ps1
+++ drop-twitter.ps1
@@ -3,5 +3,6 @@
 if (-not(Get-NetFirewallRule | where DisplayName -eq drop-twitter)) {
 	New-NetFirewallRule -DisplayName "drop-twitter" -Description "" -Enabled True
 }
+Enable-NetFirewallRule -DisplayName "drop-twitter"
 
 Set-NetFirewallRule -DisplayName "drop-twitter" -Direction Outbound -Action Block -RemoteAddress $adr -LocalAddress Any
