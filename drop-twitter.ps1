37acffd4dbc25f58f65e470867ffa77a84dffe8a
wilhellmina <x@wakiga.nl>
2021-10-30 23:19:32 +0900

    initial commit

---
+++ drop-twitter.ps1
@@ -0,0 +1,10 @@
+﻿$adr = (Resolve-DnsName twitter.com -Type A).IPaddress
+
+if (-not(Get-NetFirewallRule | where DisplayName -eq drop-twitter)) {
+	New-NetFirewallRule -DisplayName "drop-twitter" -Description "" -Enabled True
+}
+
+Set-NetFirewallRule -DisplayName "drop-twitter" -Direction Outbound -Action Block -RemoteAddress $adr -LocalAddress Any
+
+
+
